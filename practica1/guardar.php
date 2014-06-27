ffadaa33c1ba18710b7ff3d350f1f91722e13048
Jaime Fernando <fernandomargaiz@gmail.com>
2014-06-27 16:21:19 -0500

    primer commit

---
+++ practica1/guardar.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hola...";
+echo "<br/>";
+echo "Goles Equipo Local:	" .$_POST['equipo1_local'];
+echo "<br/>";
+echo "Goles Equipo Visitante:	" .$_POST['equipo2_visitante'];
+echo "<br/>";
+echo "Goles Equipo Local:	" .$_POST['equipo3_local'];
+echo "<br/>";
+echo "Goles Equipo Visitante:	" .$_POST['equipo4_visitante'];
+echo "<br/>";
+echo "Equipo elegido:	".$_POST['selectEquipo5_local'};
+echo "Goles Equipo Local:	" .$_POST['equipo5_local'];
+echo "<br/>";
+echo "Equipo elegido:	".$_POST['selectEquipo6_visitante'};
+echo "Goles Equipo Visitante:	" .$_POST['equipo6_visitante'];
+?>
